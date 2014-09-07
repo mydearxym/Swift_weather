e5985f9c20cd8e10063c87cae16b3c8b8cdc9a71
mydearxym <mydearxym@qq.com>
2014-09-07 17:50:29 +0800

    env done and learn CoreLocation

--- my-weather/ViewController.swift
+++ my-weather/ViewController.swift
@@ -7,19 +7,46 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
-                            
+class ViewController: UIViewController,CLLocationManagerDelegate {
+    let locationManger:CLLocationManager = CLLocationManager()
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        locationManger.delegate = self
+        locationManger.desiredAccuracy = kCLLocationAccuracyBest
+     
+        if(ios8()) {
+            locationManger.requestAlwaysAuthorization()
+        }
+        locationManger.startUpdatingLocation()
     }
 
+    func ios8() -> Bool {
+        return UIDevice.currentDevice().systemVersion == "8.0"
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!){
+        var location:CLLocation = locations[locations.count-1] as CLLocation
+
+        if (location.horizontalAccuracy > 0) {
+            println(location.coordinate.latitude)
+            println(location.coordinate.longitude)
+            locationManger.stopUpdatingLocation()
+        }
+    }
+    
+    func locationManager(manager: CLLocationManager!, didFailWithError error: NSError!){
+        println(error)
+    }
 
 }
 
